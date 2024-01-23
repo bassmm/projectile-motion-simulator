e85c9c58a6a6a5b93bf7d77ad35202e3bba08e61
Bassam <bassamtop123@gmail.com>
2024-01-23 18:47:53 +0300

    added function to return an array of vertial and horizontal values of s.

--- main.py
+++ main.py
@@ -8,16 +8,34 @@ angle = 45
 s_vert = []
 s_hori = []
 
-s_y = 0
-t = 0
-while s_y >= 0:
-    s_y = int(t*u*math.sin(angle) - 4.905*math.pow(t,2))
-    s_x = int(t*u*math.cos(angle))
-    s_vert.append(s_y)
-    s_hori.append(s_x)
-    t += 1
-    if s_y < 0:
-        s_vert.pop()
-print(s_vert)
-print(s_hori)
+
+def get_s_vals(u:int, angle:int):
+    s_y = 0
+    t = 0
+
+    while s_y >= 0:
+           
+        s_y = int(t*u*math.sin(angle) - 4.905*math.pow(t,2))
+        s_x = int(t*u*math.cos(angle))
+        
+        s_vert.append(s_y)
+        s_hori.append(s_x)
+        
+        t += 1
+        if s_y < 0:
+            s_vert.pop()
+            s_hori.pop()
+            s_vert.append(0)
+            t_at_0 = (u + math.sqrt(math.pow(u,2) - 4*4.905*0)) / (2*-4.905)
+            
+            # Making sure time is always positive!
+            if t_at_0 > 0:
+                t_at_0 = (u - math.sqrt(math.pow(u,2) - 4*4.905*0)) / (2*-4.905)
+               
+            s_hori.append(int(t_at_0*u*math.cos(angle)))
+
+    return s_hori, s_vert
+
+
+
 
